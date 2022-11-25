442f7f6159b3c06444c6f5c1c5e47ca944463dcf
Jan Travnicek <trawais002@gmail.com>
2022-11-25 14:04:25 +0100

    task 04.2 done

--- test/04/Solution.js
+++ test/04/Solution.js
@@ -30,7 +30,56 @@ export class Solution {
     });
   }
 
-  playBingoUntilWin() {
-    
+  firstPart() {
+    const winner = undefined;
+    for (let i = 0; i < this.input.length; i++) {
+      this.playNumber(this.input[i]);
+      const winner = this.getWinner();
+      if (typeof winner != 'undefined') {
+        return this.input[i] * this.sumOfBingo(winner);
+      }
+    }
+  }
+
+  sumOfBingo(winnerCard) {
+    let sum = 0;
+    this.bingos[winnerCard].forEach((row) => {
+      row.forEach((el) => {
+        if (el != 'X') {
+          sum += parseInt(el, 10);
+        }
+      })
+    })
+    return sum;
+  }
+
+  playNumber(number) {
+    for (let bingoCard = 0; bingoCard < this.bingos.length; bingoCard++) {
+      for (let row = 0; row < 5; row++) {
+        for (let col = 0; col < 5; col++) {
+          if (this.bingos[bingoCard][row][col] == number) {
+            this.bingos[bingoCard][row][col] = 'X';
+          }
+        }
+      }
+    }
+  }
+
+  getWinner() {
+    for (let bingoCard = 0; bingoCard < this.bingos.length; bingoCard++) {
+      const bingo = this.bingos[bingoCard];
+      // check rows
+      for (let row = 0; row < 5; row++) {
+        if (bingo[row].join('') == 'XXXXX') return bingoCard;
+      }
+      // check columns
+      for (let col = 0; col < 5; col++) {
+        const tempColumn = bingo.map((row) => {
+          return row[col];
+        });
+        if (tempColumn.join('') == 'XXXXX') return bingoCard;
+      }
+    }
+    return undefined;
   }
 }
--- test/04/task.spec.js
+++ test/04/task.spec.js
@@ -5,13 +5,12 @@ describe('Task 04', () => {
   describe('What is the power consumption of the submarine?', () => {
     it('Given example', () => {
       const solution = new Solution('./test/04/input-example.txt');
-      solution.playBingoUntilWin();
-      //assert.equal(solution.firstPart(), 4512);
+      assert.equal(solution.firstPart(), 4512);
     });
 
-    // it('Solution for the first part', () => {
-    //   const task = new Solution('./test/04/input.txt');
-    //   assert.equal(task.firstPart(), 456);
-    // });
+    it('Solution for the first part', () => {
+      const solution = new Solution('./test/04/input.txt');
+      assert.equal(solution.firstPart(), 55770);
+    });
   });
 });
