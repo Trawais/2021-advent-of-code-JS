a4d5a8408fd66e83e447d5c6ac4bd6de3ece3376
Jan Travnicek <trawais002@gmail.com>
2022-11-27 20:41:00 +0100

    task 05.1 in progress

--- test/05/Solution.js
+++ test/05/Solution.js
@@ -3,13 +3,46 @@ import * as fs from 'fs';
 export class Solution {
   constructor(filePath) {
     var fileContent = fs.readFileSync(filePath).toString('utf-8').trimEnd();
-    this.input = fileContent.split("\n").map((line) => {
-      return line;
+    this.lines = fileContent.split("\n").map((line) => {
+      let coordinates = line.split(' -> ');
+      return coordinates.map((coordinate) => {
+        let splitted = coordinate.split(',');
+        return splitted.map(number => {return parseInt(number, 10)})
+      })
     });
+    this.canvas = []
+    this.canvas[0] = [];
   }
 
   firstPart() {
-    return 1;
+     this.drawLines();
+     return 1;
+  }
+
+  drawLines() {
+    this.lines.forEach(([[x1, y1], [x2, y2]]) => {
+      if (x1 != x2 && y1 != y2) return; // skip this line; It's not horizontal nor vertical line
+      
+      // draw in x axis
+      if (x2 < x1) [x1, x2] = [x2, x1];
+      for (let x = x1; x <= x2; x++) {
+        this.drawDot(x, y1);
+      }
+      
+      // draw in y axis
+      if (y2 < y1) [y1, y2] = [y2, y1];
+      for (let y = y1; y <= y2; y++) {
+        this.drawDot(x1, y);
+      }
+    })
+  }
+
+  drawDot(x, y) {
+    if (!this.canvas[y]) this.canvas[y] = [];
+
+    if (!this.canvas[y][x]) this.canvas[y][x] = 0;
+
+    this.canvas[y][x] += 1;
   }
 
   secondPart() {
